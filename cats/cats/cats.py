6ce3bc3ea5e589503df8be90007488ca55e60414
Xinran Zhao <1259449968@qq.com>
2023-12-05 22:03:02 +0800

    problem6 solved

--- cats/cats/cats.py
+++ cats/cats/cats.py
@@ -176,54 +176,20 @@ def autocorrect(typed_word, word_list, diff_function, limit):
 
 
 def feline_fixes(typed, source, limit):
-	"""A diff function for autocorrect that determines how many letters
-	in TYPED need to be substituted to create SOURCE, then adds the difference in
-	their lengths and returns the result.
+    """"""
+    
+    def helper(typed, source, count):
+        if len(typed) == 0 or len(source) == 0:
+            return count + abs(len(typed) - len(source))
+        if typed[0] != source[0]:
+            count += 1
+        if count > limit:
+            return count
+        return helper(typed[1:], source[1:], count)
 
-	Arguments:
-		typed: a starting word
-		source: a string representing a desired goal word
-		limit: a number representing an upper bound on the number of chars that must change
-
-	>>> big_limit = 10
-	>>> feline_fixes("nice", "rice", big_limit)    # Substitute: n -> r
-	1
-	>>> feline_fixes("range", "rungs", big_limit)  # Substitute: a -> u, e -> s
-	2
-	>>> feline_fixes("pill", "pillage", big_limit) # Don't substitute anything, length difference of 3.
-	3
-	>>> feline_fixes("roses", "arose", big_limit)  # Substitute: r -> a, o -> r, s -> o, e -> s, s -> e
-	5
-	>>> feline_fixes("rose", "hello", big_limit)   # Substitute: r->h, o->e, s->l, e->l, length difference of 1.
-	5    de
-
-
-	"""
-	# BEGIN PROBLEM 6
-#     minn = min(len(typed), len(source))
-#     def helper(index, cnt):
-#         if index == minn:
-#             return cnt + abs(len(typed) - len(source))
-#         if cnt > limit:
-#             return cnt + 1
-#         if typed[index] != source[index]:
-#             cnt += 1
-#         return helper(index + 1, cnt)
-
-	min_length = min(len(typed), len(source))
-	def func(i, total): 
-		if i == min_length: 
-			total += abs(len(typed) - len(source))
-		elif total > limit: 
-			total = limit + 1
-		elif typed[i] != source[i]: 
-			return func(i + 1, total + 1)
-		else:	
-			return func(i + 1, total)
-		return total 
-	return func(0, 0) 
-
-	return helper(0, 0)
+    count = helper(typed, source, 0)
+    print("DEBUG:", count)
+    return count
 
     # END PROBLEM 6
 
