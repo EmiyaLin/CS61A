7d3b4db72a44891f872cdb7cc1ce5c4650fc2cc8
Xinran Zhao <1259449968@qq.com>
2024-02-05 11:25:26 +0800

    finish lab10 q01 q02

---
+++ lab10/lab10/lab10.scm
@@ -0,0 +1,25 @@
+(define (over-or-under num1 num2) 'YOUR-CODE-HERE
+  (cond
+    	((< num1 num2) -1)
+	((= num1 num2) 0)
+	((> num1 num2) 1)
+	)
+ ) 
+
+(define (make-adder num) 'YOUR-CODE-HERE)
+
+(define (composed f g) 'YOUR-CODE-HERE)
+
+(define (repeat f n) 'YOUR-CODE-HERE)
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b) 'YOUR-CODE-HERE)
